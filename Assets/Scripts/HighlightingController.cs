a8557917b5ec9970308f18fb61a1e780759641fc
MastWex <09dracon@gmail.com>
2024-04-13 16:43:11 +0300

    MinorBugFix
    -Пофикшен баг, когда юниты не могли наступать на клетки с ресурсами

--- Assets/Scripts/HighlightingController.cs
+++ Assets/Scripts/HighlightingController.cs
@@ -25,7 +25,7 @@ public class HighlightingController : MonoBehaviour
     private void IsUnitHighlighted(Transform highlighted)
     {
         if (_highlightedBeforeUnit != null) _highlightedBeforeUnit.IsHighlighted = false;
-        if (_placementManager.gridWithObjectsInformation[_hexGrid.InLocalCoords(highlighted.position).x, _hexGrid.InLocalCoords(highlighted.position).y] == null) // На выделенной клетке ничего нет
+        if (_placementManager.gridWithObjectsInformation[_hexGrid.InLocalCoords(highlighted.position).x, _hexGrid.InLocalCoords(highlighted.position).y] == null) // На подсвеченной клетке ничего нет
         {
             isAnyUnitHighlighted = false;
             highlightedUnit = null;
@@ -33,7 +33,7 @@ public class HighlightingController : MonoBehaviour
         else 
         {
             highlightedUnit = _placementManager.gridWithObjectsInformation[_hexGrid.InLocalCoords(highlighted.position).x, _hexGrid.InLocalCoords(highlighted.position).y].GetComponent<UnitDescription>();
-            if (highlightedUnit != null) // На выделенной клетке есть юнит
+            if (highlightedUnit != null) // На подсвеченной клетке есть юнит
             {
                 isAnyUnitHighlighted = true;
                 highlightedUnit.IsHighlighted = true;
@@ -42,7 +42,7 @@ public class HighlightingController : MonoBehaviour
             else
             {
                 isAnyUnitHighlighted = false;
-                highlightedUnit.IsHighlighted = false;
+                if (highlightedUnit != null) highlightedUnit.IsHighlighted = false;
             }
         }
 
@@ -50,9 +50,24 @@ public class HighlightingController : MonoBehaviour
     private void DistanceBetweenSelectedAndHighlighted(Transform highlighted)
     {
         if (_mouseSelection.selected == null) return;
-        distanceBetweenSelectedAndHighlighted =
-            Mathf.Abs(_hexGrid.InLocalCoords(_mouseSelection.selected.position).x - _hexGrid.InLocalCoords(highlighted.position).x)
-            + Mathf.Abs(_hexGrid.InLocalCoords(_mouseSelection.selected.position).y - _hexGrid.InLocalCoords(highlighted.position).y);
+        int deltaY = _hexGrid.InLocalCoords(highlighted.position).y - _hexGrid.InLocalCoords(_mouseSelection.selected.position).y;
+        int deltaX = _hexGrid.InLocalCoords(highlighted.position).x - _hexGrid.InLocalCoords(_mouseSelection.selected.position).x;
+        float absDeltaY = Mathf.Abs(deltaY);
+        float absDeltaX = Mathf.Abs(deltaX);
+        distanceBetweenSelectedAndHighlighted = (int)absDeltaX + (int)absDeltaY;
+        Debug.Log(_hexGrid.InLocalCoords(_mouseSelection.selected.position).y);
+        if (deltaX == 0 || deltaY == 0) return;
+        if (_hexGrid.InLocalCoords(_mouseSelection.selected.position).y % 2 == 0) // Стоим на чётной строке
+        {
+            if (deltaX < 0) distanceBetweenSelectedAndHighlighted -= Mathf.CeilToInt(absDeltaY / 2);
+            else distanceBetweenSelectedAndHighlighted -= Mathf.FloorToInt(absDeltaY / 2);
+        }
+        else
+        {
+            if (deltaX > 0) distanceBetweenSelectedAndHighlighted -= Mathf.CeilToInt(absDeltaY / 2);
+            else distanceBetweenSelectedAndHighlighted -= Mathf.FloorToInt(absDeltaY / 2);
+        }
+        Debug.Log((absDeltaX, absDeltaY, Mathf.Clamp(Mathf.FloorToInt(Mathf.Max(absDeltaX, absDeltaY) / 2), 0, (int)Mathf.Min(absDeltaX, absDeltaY))));
     }
     private void OnHighlightedChanged(Transform highlighted)
     {
