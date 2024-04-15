43744d004a50684b4b2a98d96d4c0cb6189da863
MastWex <09dracon@gmail.com>
2024-04-15 18:38:39 +0300

    Interface
    -Добавлена сцена MainMenu
    -Добавлены префабы кнопки, заголовка окна, экрана меню(Префаб экрана настроен так, чтобы он масштабировался под количество кнопок, также, кнопки в нём изменяют свой размер под него)
    -Добавлен префаб ScreensController (вытащить на сцену, чтобы работал), который будет отвечать за показ менюшек и переходы между сценами. В нём есть публичные методы: LoadScene - принимает сцену, которую нужно загрузить; ShowScreen - принимает экран меню, который нужно показать, закрывает все остальные экраны меню на сцене(всё, у чего есть компонент MenuScreen); Quit - выйти из игры. Также у него есть публичная переменная showOnStart. Если в неё положить экран меню, то он отобразится на старте, а все остальные закроются, если в неё ничего не положить, то все экраны на старте закроются. Для корректной работы ScreenController, все объекты, у которых есть MenuScreen должны быть активны на старте.

---
+++ Assets/Scripts/ScreensController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreensController : MonoBehaviour
+{
+    public MenuScreen showOnStart = null;
+    private List<MenuScreen> _menuScreensOnTheScene;
+    private void Start()
+    {
+        _menuScreensOnTheScene = new List<MenuScreen>(FindObjectsOfType<MenuScreen>());
+        ShowScreen(showOnStart);
+    }
+    public void LoadScene(Scene sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad.name);
+    }
+    public void ShowScreen(MenuScreen menuScreenToShow)
+    {
+        foreach (MenuScreen menuScreen in _menuScreensOnTheScene)
+        {
+            menuScreen.gameObject.SetActive(false);
+        }
+        menuScreenToShow?.gameObject.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
