acf8ffef1041663363e7d894282ade725d42fec3
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-04-29 22:36:28 +0300

    BaseDarknessDamage
    - Теперь тьма наносит урон каждый ход постройкам и юнитам
    - Для работы скрипта я навесил на всех юнитов и постройки коллайдеры и RigidBody, которые не обрабатываются физикой.

--- Assets/Scripts/Darkness/Darkness.cs
+++ Assets/Scripts/Darkness/Darkness.cs
@@ -1,8 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Darkness : ObjectOnGrid
 {
     public float Damage;
+    private bool _isSomethingInDarkness=false;
+    private GameObject _objectInDarkness;
+
+    private void Awake()
+    {
+        TurnManager.onTurnChanged += OnTrurnChanged;
+    }
+    private void OnTriggerStay(Collider other) // Проверка есть ли что-либо в этой тьме
+    {
+        _objectInDarkness = other.gameObject;
+        _isSomethingInDarkness = true;
+    }
+
+    private void OnTrurnChanged()
+    {
+        if (_isSomethingInDarkness == true)
+        {
+            if (_objectInDarkness.TryGetComponent(out FacilityHealth facilityHealth) != false) // Пробуем найти у объекта во тьме компонент FacilityHealth
+            {
+                facilityHealth.ApplyDamage(Damage);
+            }
+            else if (_objectInDarkness.TryGetComponent(out UnitHealth unitHealth)) // Иначе пробуем найти у него компонент UnitHealth
+            {
+                unitHealth.ApplyDamage(Damage);
+            }
+            _isSomethingInDarkness = false;
+        }
+    }
 }
--- Assets/Scripts/Light/LightTransporter.cs
+++ Assets/Scripts/Light/LightTransporter.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System;
-using System.Linq;
 
 public class LightTransporter : MonoBehaviour {
     public LightTransporter Source;
