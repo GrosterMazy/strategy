31e9c3c08593260ac56eabd2b9263abb27036595
GrosterMazy <78735997+GrosterMazy@users.noreply.github.com>
2024-05-18 16:39:12 +0300

    ResourceWeights Update

--- Assets/Scripts/ResourcesWeights.cs
+++ Assets/Scripts/ResourcesWeights.cs
@@ -8,5 +8,6 @@ public static class ResourcesWeights
 
     static ResourcesWeights() {
         ResourcesWeightsPerItemTable["Tree"] = 1;  ResourcesWeightsPerItemTable["Wood"] = 1; ResourcesWeightsPerItemTable["Ore"] = 1;
-        ResourcesWeightsPerItemTable["Seed"] = 1; ResourcesWeightsPerItemTable["Weed"] = 1; } // запишите сюда значения масс всех ресурсов в игре по образцу
+        ResourcesWeightsPerItemTable["Seed"] = 1; ResourcesWeightsPerItemTable["Food"] = 1; ResourcesWeightsPerItemTable["Light"] = 0;
+        ResourcesWeightsPerItemTable["Darkness"] = 0;} // запишите сюда значения масс всех ресурсов в игре по образцу
 }
