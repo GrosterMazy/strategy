4708c341e0a8640b998ca7ef745ae2c6b0e73972
WenderOk <127617256+WenderOk@users.noreply.github.com>
2024-05-23 20:40:21 +0300

    Main Menu & Sun

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private Transform SettingsWindow;
     [SerializeField] private Transform MainMenuWindow;
-    public void StartGane() { }
+    public void StartGane() { SceneManager.LoadScene("MainGameScene"); }
     public void OpenSettings() { FindObjectOfType<MenuCamera>().objectToFollow = SettingsWindow; }
     public void CloseSettings() { FindObjectOfType<MenuCamera>().objectToFollow = MainMenuWindow; }
     public void QuitGame() { Application.Quit(); }
---
+++ Assets/Scripts/Menu/SunControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SunControl : MonoBehaviour
+{
+    [SerializeField] private float SunSpeed;
+    private void OnEnable() { TurnManager.NightStarts += StartNight; TurnManager.DayStarts += StartDay; }
+    private void OnDisable() { TurnManager.NightStarts -= StartNight; TurnManager.DayStarts -= StartDay; }
+    private void StartNight()  { transform.rotation = Quaternion.Euler(0, 150, 0); }
+    private void StartDay() { transform.rotation = Quaternion.Euler(50, 150, 0); }
+    
+        
+    
+}
